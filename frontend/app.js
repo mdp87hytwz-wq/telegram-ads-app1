f922ae0919e8b8d6a724f9736544a5050e532a58
apple <apple@apples-MacBook-Pro.local>
2026-06-28 08:25:26 -0700

    Fix f-amount keyboard

--- frontend/app.js
+++ frontend/app.js
@@ -289,7 +289,7 @@ function viewBudget() {
 
       <div class="field">
         <label>Add to your budget :</label>
-        <input type="number" id="f-amount" placeholder="10000" />
+        <input type="text" inputmode="numeric" id="f-amount" placeholder="10000" />
       </div>
 
       <label style="font-weight:700;font-size:14px;">Payment method :</label>
