04d2bfdedfdaf2788d19f443654500316fd060d9
apple <apple@apples-MacBook-Pro.local>
2026-06-27 22:00:18 -0700

    Redeploy bot with confirmpay

--- bot/bot.js
+++ bot/bot.js
@@ -101,3 +101,4 @@ bot.onText(/\/broadcast (.+)/, async (msg, match) => {
 
 console.log('Bot is running in polling mode.');
 // redeploy Sat Jun 27 21:58:42 PDT 2026
+// redeploy Sat Jun 27 22:00:18 PDT 2026
